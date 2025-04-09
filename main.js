b957e4c934eaa2cc814a47bc53169dc41d26d25b
Xamidullo Xudoyberdiyev <khkhamidullo@gmail.com>
2025-04-09 15:51:44 +0500

    merged

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 import express from "express";
 
-
-
-const app  = express()
+const app = express();
 
 app.use(express.json());
 
-
 // xamidullo.com/
 
-app.get("/",(req, res)=>{
-res.send("Assalomu alaykum♂️")
-})
+app.get("/", (req, res) => {
+  res.send("Assalomu alaykum♂️");
+});
 
-
-app.listen(4000, ()=>{
-  console.log("Server runnin on port 4000")
-})
+app.listen(4000, () => {
+  console.log("Server runnin on port 4000");
+});
