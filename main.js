cdc781f779f513482570ffcb1958c9665a96ef7f
Xamidullo Xudoyberdiyev <khkhamidullo@gmail.com>
2025-04-09 15:49:32 +0500

    simple super app

---
+++ main.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+
+
+const app  = express()
+
+app.use(express.json());
+
+
+// xamidullo.com/
+
+app.get("/",(req, res)=>{
+res.send("Assalomu alaykum♂️")
+})
+
+
+app.listen(4000, ()=>{
+  console.log("Server runnin on port 4000")
+})
